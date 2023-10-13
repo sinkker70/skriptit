1b436e97b80f0ef8da54a1a99bf7441c8dfd137a
Erkki Sinkko <erkki.sinkko@hyria.fi>
2023-10-13 14:13:48 +0300

    kokeiluja

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Hello World", "jeeeee!")
+console.info("mitä?")
+
+y = 110; 
+console.log(y)
+
+big = 34928758923759823758.0;
+let e; 
+console.log("wetwerw", e)
+
+hop = {
+    y: 8, 
+    x: 43
+}
+console.table(hop);
